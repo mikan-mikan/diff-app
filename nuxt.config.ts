9de540e28f606b14a2f5b7649bceb7af2df40a9b
mikan-mikan <51882748+mikan-mikan@users.noreply.github.com>
2024-02-27 22:04:31 +0900

    環境構築｜Sass, リセットCSSを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,14 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true }, // 開発ツールを有効にする（Vue Devtoolsなど）
   modules: ['@nuxtjs/eslint-module'], // NuxtJSのESLintモジュールをプロジェクトに追加する,eslintrc.jsonの読み込み設定は不要
+  css: ['normalize.css', '@/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/_variables.scss";`, // パーシャルファイルを読み込む
+        },
+      },
+    },
+  },
 });
